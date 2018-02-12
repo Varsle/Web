203c09147a7d91a3e9c2f94a8e2944c67b830b53
Wiklem <admin@wiklem.no>
2018-02-12 13:55:24 +0100

    Category modularisation

--- src/containers/Fire/Fire.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Overview} from "../../components/Overview/Overview";
-
-const Fire = () =>
-    <div>
-        <Overview name={"Brannvesen"}/>
-    </div>
-
-export {Fire};
--- src/containers/Medic/Medic.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Overview} from "../../components/Overview/Overview";
-
-const Medic = () =>
-    <div>
-        <Overview name={"Ambulanse"}/>
-    </div>
-
-export {Medic};
