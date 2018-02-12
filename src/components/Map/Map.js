a8407b3fef550e9a403f393b74bcd2c5017c4150
Wiklem <admin@wiklem.no>
2018-02-12 13:36:07 +0100

    Disable Google API in dev.

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,9 +3,15 @@ import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import {conf} from "../../conf/conf";
 
+/** Load API KEY in production mode **/
+let url = "https://maps.googleapis.com/maps/api/js?key=" + conf.google + "&v=3.exp&libraries=geometry,drawing,places"
+if (process.env.NODE_ENV === `development`) {
+    url = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+}
+
 const Map = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key="+conf.google+"&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: url,
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
--- src/components/Map/MapCluster.js
+++ src/components/Map/MapCluster.js
@@ -4,9 +4,15 @@ import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps"
 import {conf} from "../../conf/conf";
 import {MarkerClusterer} from "react-google-maps/lib/components/addons/MarkerClusterer";
 
+/** Load API KEY in production mode **/
+let url = "https://maps.googleapis.com/maps/api/js?key=" + conf.google + "&v=3.exp&libraries=geometry,drawing,places"
+if (process.env.NODE_ENV === `development`) {
+    url = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+}
+
 const CreateCluster = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=" + conf.google + "&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: url ,
         loadingElement: <div style={{height: `100%`}}/>,
         containerElement: <div style={{height: `400px`}}/>,
         mapElement: <div style={{height: `100%`}}/>,
